abd498b0dda2cd465b08ed50a46778909fabbf27
Kartikey027 <saxenakartikey208@gmail.com>
2024-06-14 13:01:13 +0530

    Task 2

--- Student_Grade_Calculator.java
+++ Student_Grade_Calculator.java
@@ -1,49 +1,50 @@
 import java.util.*;
+
 public class Student_Grade_Calculator {
-public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    // Get the number of subjects
-    System.out.print("Enter the number of subjects: ");
-    int numSubjects = scanner.nextInt();
+        // Get the number of subjects
+        System.out.print("Enter the number of subjects: ");
+        int Tsubjects = sc.nextInt();
 
-    // Array to store marks for each subject
-    int[] marks = new int[numSubjects];
+        // Array to store marks for each subject
+        int[] marks = new int[Tsubjects];
 
-    // Input marks for each subject
-    for (int i = 0; i < numSubjects; i++) {
-        System.out.print("Enter marks for subject " + (i + 1) + ": ");
-        marks[i] = scanner.nextInt();
-    }
+        // Input marks for each subject
+        for (int i = 0; i < Tsubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+        }
 
-    // Calculate total marks
-    int totalMarks = 0;
-    for (int mark : marks) {
-        totalMarks += mark;
-    }
+        // Calculate total marks
+        int Tmarks = 0;
+        for (int i : marks) {
+            Tmarks += i;
+        }
 
-    // Calculate average percentage
-    double averagePercentage = (double) totalMarks / numSubjects;
+        // Calculate average percentage
+        double per = (double) Tmarks / Tsubjects;
 
-    // Calculate grade based on average percentage
-    char grade;
-    if (averagePercentage >= 90) {
-        grade = 'A';
-    } else if (averagePercentage >= 80) {
-        grade = 'B';
-    } else if (averagePercentage >= 70) {
-        grade = 'C';
-    } else if (averagePercentage >= 60) {
-        grade = 'D';
-    } else {
-        grade = 'F';
-    }
+        // Calculate grade based on average percentage
+        char grade;
+        if (per >= 90) {
+            grade = 'A';
+        } else if (per >= 80) {
+            grade = 'B';
+        } else if (per >= 70) {
+            grade = 'C';
+        } else if (per >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
 
-    // Display results
-        System.out.println("Total Marks: " + totalMarks);
-        System.out.println("Average Percentage: " + averagePercentage + "%");
+        // Display results
+        System.out.println("Total Marks: " + Tmarks);
+        System.out.println("Average Percentage: " + per + "%");
         System.out.println("Grade: " + grade);
 
-        scanner.close();
+        sc.close();
     }
 }
